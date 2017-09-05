16e2f6a0364f7af7f9104ffef25f4e9b6b50634a
beanstown106 <nbenis106@gmail.com>
2017-09-05 14:35:10 +0800

    incall vibration options [2/3] *allow setting vibration when call is connected *allow setting vibration when call is disconnected *allow setting vibration for call waiting *this works with google and aosp dialer :)
    Change-Id: I1dcdf20d022df94cf798548f0c3e2d524f8b21ca
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- src/com/android/server/telecom/Ringer.java
+++ src/com/android/server/telecom/Ringer.java
@@ -29,7 +29,9 @@ import android.media.Ringtone;
 import android.media.VolumeShaper;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.os.Vibrator;
+import android.provider.Settings;
 
 import com.android.internal.annotations.VisibleForTesting;
 
@@ -424,6 +426,11 @@ public class Ringer {
 
         stopRinging();
 
+        if (Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.VIBRATE_ON_CALLWAITING, 0, UserHandle.USER_CURRENT) == 1) {
+            vibrate(200, 300, 500);
+        }
+
         if (mCallWaitingPlayer == null) {
             Log.addEvent(call, LogUtils.Events.START_CALL_WAITING_TONE, reason);
             mCallWaitingCall = call;
@@ -523,5 +530,12 @@ public class Ringer {
         return mSystemSettingsUtil.canVibrateWhenRinging(context)
             || (mSystemSettingsUtil.applyRampingRinger(context)
                 && mSystemSettingsUtil.enableRampingRingerFromDeviceConfig());
+    }
+
+    public void vibrate(int v1, int p1, int v2) {
+        long[] pattern = new long[] {
+            0, v1, p1, v2
+        };
+        ((Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE)).vibrate(pattern, -1);
     }
 }
