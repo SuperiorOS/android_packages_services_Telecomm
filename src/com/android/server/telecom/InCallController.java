866dcdade0a76e2353e8081babdebbf84b652a43
xyyx <xyyx@mail.ru>
2017-09-05 16:02:21 +0800

    Use double vibration on call hang-up
    Change-Id: I79f44f591ede74eab50d0f9c21ebf2de4ae44923
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- src/com/android/server/telecom/InCallController.java
+++ src/com/android/server/telecom/InCallController.java
@@ -922,7 +922,7 @@ public class InCallController extends CallsManagerListenerBase {
             vibrate(100, 200, 0);
         } else if (oldState == CallState.ACTIVE && newState == CallState.DISCONNECTED
                 && vibrateOnDisconnect) {
-            vibrate(100, 200, 0);
+            vibrate(50, 100, 50);
         }
         updateCall(call);
     }
